dcded40371a5a42a9a3e972787934e12fe2df3d5
Tim Fielder <fiel1139@gmail.com>
2018-06-30 22:18:47 -0600

    returns to basic definitions in round_class.

--- lib/round_class.rb
+++ lib/round_class.rb
@@ -1,37 +1,17 @@
 require "./lib/card_class.rb"
 require "./lib/guess_class.rb"
-require ".lib/deck_class.rb"
+require "./lib/deck_class.rb"
 
 class Round
 
-attr_accessor :deck
+  attr_accessor :deck
 
-def initialize(deck)
-  @deck = deck
-end
+  def initialize(deck)
+    @deck = deck
+  end
 
-def deck
-  @deck
-end
-
-def #guesses
-
-end
-
-def #current_card
-#should return the current card
-end
-
-def record_guess
-
-end
-
-def #count
-
-end
-
-def #feedback
-
-end
+  def deck
+    @deck
+  end
 
 end
