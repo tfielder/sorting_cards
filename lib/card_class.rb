8c9b1d2ff53d37834d9ae1b0523f442b82b5ef67
Tim Fielder <fiel1139@gmail.com>
2018-06-30 18:26:59 -0600

    sets up the card class initialize, and getter methods for suit and value

---
+++ lib/card_class.rb
@@ -0,0 +1,21 @@
+class Card
+
+attr_accessor :value
+attr_accessor :suit
+
+def initialize(value, suit)
+  @value = value
+  @suit = suit
+end
+
+def suit
+  @suit
+end
+
+def value
+  @value
+end
+
+
+
+end
