2381dc6135e591e8ccdcc64d44db7c0afb3237a5
Tim Fielder <fiel1139@gmail.com>
2018-07-01 10:58:30 -0600

    adds test for returning guesses array.

--- test/round_class_test.rb
+++ test/round_class_test.rb
@@ -24,8 +24,11 @@ class RoundClassTest < Minitest::Test
   end
 
   def test_for_returning_guesses
-    skip
-    assert_equal
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal [], round.guesses
   end
 
   def test_for_returning_current_card
