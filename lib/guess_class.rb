53da65287c3f2b4e9b1989fec7e356a4a4b38a3d
Tim Fielder <fiel1139@gmail.com>
2018-06-30 19:39:39 -0600

    adds the placeholders for the methods in guess_class.

--- lib/guess_class.rb
+++ lib/guess_class.rb
@@ -1,3 +1,20 @@
-class Guess
+require "./lib/card_class.rb"
 
+class Guess
+  def initialize(response, card)
+    @response = response
+    @card = card
+  end
+
+  def response
+    @response
+  end
+
+  def correct?
+
+  end
+
+  def feedback
+
+  end
 end
