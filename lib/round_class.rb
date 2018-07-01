587b0deb5b2a592d40ebaad26a893f816ffd1c8d
Tim Fielder <fiel1139@gmail.com>
2018-07-01 08:01:01 -0600

    adds additional definitions to round_class methods and working code.

--- lib/round_class.rb
+++ lib/round_class.rb
@@ -5,13 +5,45 @@ require "./lib/deck_class.rb"
 class Round
 
   attr_accessor :deck
+  attr_accessor :guesses
+  attr_accessor :guess_count
 
   def initialize(deck)
     @deck = deck
+    @guesses = []
+    @guess_count = 0
   end
 
   def deck
     @deck
+  end
+
+  def guesses
+    @guesses
+  end
+
+  def current_card
+    #? What should this do?
+  end
+
+  def record_guess(guess)
+    @guesses.unshift(guess)
+  end
+
+  def first
+    @guesses.index[0]
+  end
+
+  def last
+    @guesses.index[-1]
+  end
+
+  def number_correct
+    #should tell you how many you got right?
+  end
+
+  def percent_correct
+    #something / @guess_count
   end
 
 end
