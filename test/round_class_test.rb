ab3fa8185e02a9bf3317b8e417c35a7575b17c56
Tim Fielder <fiel1139@gmail.com>
2018-06-30 21:50:06 -0600

    creates framework for Round Class test.

---
+++ test/round_class_test.rb
@@ -0,0 +1,26 @@
+require "./lib/card_class.rb"
+require "./lib/guess_class.rb"
+require "./lib/deck_class.rb"
+require "./lib/round_class.rb"
+require "minitest/autorun"
+require "minitest/pride"
+
+class RoundClassTest < Minitest::Test
+
+  def test_initiates_a_class
+    skip
+
+    assert_instance_of
+  end
+
+  def test_
+    skip
+    assert_equal
+  end
+
+  def test_
+    skip
+    assert_equal
+  end
+
+end
