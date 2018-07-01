19c15a00e4af14818bdd2a421d8121d33daf9c8a
Tim Fielder <fiel1139@gmail.com>
2018-06-30 19:34:38 -0600

    gives code for tests on initializing, giving value, and suit.

--- test/card_class_test.rb
+++ test/card_class_test.rb
@@ -5,6 +5,20 @@ require "minitest/pride"
 class CardClassTest < Minitest::Test
 
   def test_initiates_a_class
-    
+    card_example = Card.new("","")
+    assert_instance_of Card, card_example
+  end
+
+  def test_class_gives_suit
+    card_example = Card.new("5","hearts")
+    suit = card_example.suit
+    assert_equal "hearts", suit
+  end
+
+  def test_class_gives_value
+    card_example = Card.new("5","hearts")
+    value = card_example.value
+    assert_equal "5", value
+  end
 
 end
