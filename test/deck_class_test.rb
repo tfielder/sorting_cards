8629e490f4b5bda6fd8e17225163e371825c2223
Tim Fielder <fiel1139@gmail.com>
2018-07-01 22:57:57 -0600

    adds the test for the sorting method.

--- test/deck_class_test.rb
+++ test/deck_class_test.rb
@@ -30,4 +30,17 @@ class DeckClassTest < Minitest::Test
     assert_equal 3, deck_example.count
   end
 
+  def test_sort_works
+    skip
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("Jack", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("Ace", "Spades")
+    card_5 = Card.new("Ace", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+
+    sorted = deck.sort
+    assert_equal [card_1, card_3, card_2, card_5, card_4], sorted
+  end
+
 end
