6774dc7c46cd5bc5edcbfb6213ea006016fc693c
Tim Fielder <fiel1139@gmail.com>
2018-07-02 23:44:25 -0600

    adds definition to algorithm for sorting.

--- lib/deck_class.rb
+++ lib/deck_class.rb
@@ -17,14 +17,55 @@ def count
   number_of_cards = @cards.count
 end
 
+def change_deck_to_numerical_values
+  #mutates the deck of cards to numerical values
+  @cards.map do |e|
+    value = cards[e].value
+
+    suit = cards[e].suit
+    puts "Your card #{value} : #{suit}"
+  end
+end
+
+def change_deck_to_face_values
+  #undoes the change_deck_to_numerical_values
+end
+
 def sort
-    #take the deck
-    @cards
     #define values
-    royalty = {"Jack" = "11", "Queen" = "12", "King" = "13", "Ace" = "14"}
-    #sort the deck by value
-    @cards.map each do |card|
+    card_values = {
+      "2": 2
+      "3": 3
+      "4": 4
+      "5": 5
+      "6": 6
+      "7": 7
+      "8": 8
+      "9": 9
+      "10": 10
+      "Jack": 11,
+      "Queen": 12,
+      "King": 13,
+      "Ace": 14}
+
+      suit_values = {
+      "Clubs": 1
+      "Diamonds": 2
+      "Hearts": 3
+      "Spades": 4
+      }
+
+
+
+    sorted_array = []
+    #sort the deck by value for each card
+
+    @cards.each each do |card|
       #compare value of card before to card
+        #insert if value is less than value at index.
+
+
+
       #return index of card unless nil
     end
     #sort the deck by suit
